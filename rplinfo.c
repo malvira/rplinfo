537daffda313c7bce43659d071cdf0837c742b13
Mariano Alvira <mar@devl.org>
2013-06-15 17:29:41 -0400

    start of new length/index api (to fix the problems with the old way)

--- rplinfo.c
+++ rplinfo.c
@@ -58,92 +58,48 @@ uint16_t create_route_msg(char *buf, uip_ds6_route_t *r)
 
 RESOURCE(routes, METHOD_GET, "rplinfo/routes", "title=\"RPL route info\";rt=\"Data\"");
 
-static volatile uint8_t cur_route_entry;
-static volatile uint8_t r_entry_char_skip;
-
 void
 routes_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
   int32_t strpos = 0;
   uip_ds6_route_t *r;
-  const char *route_header = "{\"routes\":[";
-  const char *route_footer = "]}";
   volatile uint8_t i;
-  char entry[MAX_ENTRY_LEN];
-  uint16_t entry_len;
 
-  PRINTF("pref size %d\n\r", preferred_size);
+  size_t len = 0;
+  uint8_t index;
+  const char *pstr;
+  uint8_t count;
 
-  if (*offset == 0) {
-    /* send header */    
-    PRINTF("do header\n\r");
-    strpos += snprintf((char *)buffer+strpos, preferred_size-strpos+1, route_header);
-    cur_route_entry = 0;
-    r_entry_char_skip = 0;
-  } 
-
-  PRINTF("offset %d\n\r", *offset);
-  PRINTF("cur_route_entry %d\n\r", cur_route_entry);
-  PRINTF("entry_char_skip %d\n\r", r_entry_char_skip);
-  
-  /* seek to the current entry */
-  for(r = uip_ds6_route_list_head(); r != NULL; r = list_item_next(r), i++) {
-    if (i == cur_route_entry) {
-      break;
+  if ((len = REST.get_query_variable(request, "index", &pstr))) {
+    index = (uint8_t)atoi(pstr);
+    /* seek to the route entry and return it */
+    i = 0;
+    for(r = uip_ds6_route_list_head(); r != NULL; r = list_item_next(r), i++) {
+      if (i == index) {
+	break;
+      }
     }
-  }
+    strpos = create_route_msg(buffer, r);
 
-  /* fill buffer until past preferred size */
-  for(; r != NULL; r = list_item_next(r)) {
-
-    entry_len = create_route_msg(entry, r);
-
-    strpos += snprintf((char * )buffer+strpos, preferred_size-strpos+1, &(entry[r_entry_char_skip]));
-    if(r_entry_char_skip != 0) { r_entry_char_skip = 0; } 
-    if(list_item_next(r) != NULL) {
-      strpos += snprintf((char * )buffer+strpos, preferred_size-strpos+1, ",");
-    }
-    if (strpos >= preferred_size) {
-      break;
-    }
-    cur_route_entry++;
-  }      
-  
-  if (strpos > preferred_size)
-  {
-    r_entry_char_skip = entry_len - (strpos - preferred_size) + 1;
-    strpos = preferred_size;
   } else {
-    r_entry_char_skip = 0;
+    /* index not provided */
+    /* count the number of routes and return the total */
+    count = 0;
+    for(r = uip_ds6_route_list_head(); r != NULL; r = list_item_next(r)) {
+      count++;
+    }
+    strpos += snprintf((char *)buffer, preferred_size, "%d", count);
   }
-  
-  if (r == NULL)
-  {
-    /* Signal end of resource representation. */
-    PRINTF("signal end\n\r");
-    strpos += snprintf((char *)buffer+strpos, preferred_size-strpos+1, route_footer);
-    *offset = -1;
-  } else {
-    *offset += strpos;
-  }      
-  
-  /* snprintf() does not adjust return value if truncated by size. */
-  if (strpos > preferred_size)
-  {
-    strpos = preferred_size;
-  }
+
+  *offset = -1;
   
   REST.set_response_payload(response, buffer, strpos);
 
 }
 
 
-/* send { "parents" : [ 
-/*  {"eui":"00050c2a8c9d4ea0","pref":"true","etx":124},*/
-/* ] } */
-
+/*  {"eui":"00050c2a8c9d4ea0","pref":"true","etx":124}*/
 /* length of an neighbor entry, must be fixed width */
-/* includes a trailing comma */
 uint16_t create_parent_msg(char *buf, rpl_parent_t *parent, uint8_t preferred)
 {
 	uint8_t n = 0;
@@ -220,7 +176,7 @@ parents_handler(void* request, void* response, uint8_t *buffer, uint16_t preferr
       if(parent->next != NULL) {
 	strpos += snprintf((char * )buffer+strpos, preferred_size-strpos+1, ",");
       }
-      if (strpos >= preferred_size) {
+      if (strpos > preferred_size) {
 	break;
       }
       cur_neigh_entry++;
